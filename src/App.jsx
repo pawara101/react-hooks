d00942fcb4751415106ad5378ff1f53b74e9368b
pawara101 <pawara101dassanayake@gmail.com>
2024-09-02 01:24:41 +0530

    UseReducer Hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {StateTutorial} from "./components/hooks/StateTutorial.jsx";
+import { ReducerTutorial } from './components/hooks/ReducerTutorial.jsx';
 
 function App() {
 
   return (
     <>
       <div className="card">
-            <StateTutorial></StateTutorial>
+            <ReducerTutorial></ReducerTutorial>
       </div>
     </>
   )
---
+++ src/components/hooks/ReducerTutorial.jsx
@@ -0,0 +1,31 @@
+import React, { useReducer } from 'react';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return { count: state.count + 1, showText: state.showText };
+        case 'toggleShowText':
+            return { count: state.count, showText: !state.showText };
+        default:
+            return state;
+    }
+};
+
+export const ReducerTutorial = () => {  // Capitalized the component name
+    const [state, dispatch] = useReducer(reducer, { count: 0, showText: true });
+
+    return (
+        <div>
+            <h1>{state.count}</h1>
+            <button
+                onClick={() => {
+                    dispatch({ type: 'INCREMENT' });
+                    dispatch({ type: 'toggleShowText' });
+                }}
+            >
+                Click Here
+            </button>
+            {state.showText && <p>This an Even Value</p>}
+        </div>
+    );
+};
