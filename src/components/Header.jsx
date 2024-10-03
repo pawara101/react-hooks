a1fc94c6421e65fd01c6c25baaa28a7192ef636f
pawara101 <pawara101dassanayake@gmail.com>
2024-10-03 08:35:06 +0530

    UseCallback Tutorial 1.1

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    console.log("Header Rendered");
+    return(
+        <div>
+            <h2>Header</h2>
+        </div>
+    )
+}
--- src/hooks/CallbackTutorial.jsx
+++ src/hooks/CallbackTutorial.jsx
@@ -1,14 +1,14 @@
 //useCallback lets  cache a function definition between re-renders
 import React, { useState } from 'react';
-
+import { Header } from '../components/Header';
 export function CallbackTutorial() {
     const [count, setCount] = useState(0);
 
     return (
-        <div>
-            <h1>CallBack</h1>
-            <h2>{count}</h2>
-            <button onClick={() => setCount(count + 1)}>+</button>
-        </div>
+        <>
+            <Header></Header>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}>Add</button>
+        </>
     );
 }
